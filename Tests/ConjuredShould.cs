a2a3a8321e6ed45e767bb079ecab7d57253024fb
Ljubica Ivanovska <livanovska@buyatab.com>
2020-05-07 12:23:55 -0700

    addin untracked files

---
+++ Tests/ConjuredShould.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using csharp.Constants;
+
+namespace csharp
+{
+    [TestFixture]
+    public class ConjuredShould
+    {
+        [TestCase(1, 10, ExpectedResult = 0, TestName = "QualityShouldNotBeNegative")]
+        [TestCase(50, 10, ExpectedResult = 50, TestName = "QualityShouldBeMax50")]
+        [TestCase(7, 10, ExpectedResult = 5)]
+        [TestCase(12, 10, ExpectedResult = 10)]
+        public int ConjuredQualityShould(int quality, int sellin)
+        {
+            Item item = GetItem(ItemName.Conjured, quality, sellin);
+            IList<Item> items = new List<Item> { item };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+
+            return items[0].Quality;
+        }
+
+
+        private Item GetItem(string name, int quality, int sellin)
+        {
+            return new Item
+            {
+                Name = name,
+                SellIn = sellin,
+                Quality = quality
+            };
+        }
+
+    }
+}
